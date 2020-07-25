63fde8c355ab0359247000eb05330ebf3c8fd90c
prabhat singh <prabhat.singh.cer15@itbhu.ac.in>
2020-07-25 19:37:29 +0530

    Commiting codes

---
+++ src/main/java/com/prabhat/Coronavirustracker/Controllers/HomeController.java
@@ -0,0 +1,29 @@
+package com.prabhat.Coronavirustracker.Controllers;
+
+import com.prabhat.Coronavirustracker.models.LocationStats;
+import com.prabhat.Coronavirustracker.service.CoronavirusDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    CoronavirusDataService coronavirusDataService;
+
+    @GetMapping("/")
+    public String home(Model model){
+        List<LocationStats> allStats = coronavirusDataService.getAllstats();
+        int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+        int totalNewCases = allStats.stream().mapToInt(stat -> stat.getDiffFromPrevDay()).sum();
+        model.addAttribute("locationStats",allStats);
+        model.addAttribute("totalReportedCases",totalReportedCases);
+        model.addAttribute("totalNewCases",totalNewCases);
+
+        return "home";
+    }
+}
---
+++ src/main/java/com/prabhat/Coronavirustracker/CoronavirusTrackerApplication.java
@@ -0,0 +1,18 @@
+package com.prabhat.Coronavirustracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class CoronavirusTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronavirusTrackerApplication.class, args);
+	}
+
+}
